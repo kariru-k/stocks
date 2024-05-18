cae71cb245911a16095cdd17d45cb748f10c7b7a
kariru-k <karirukeith@gmail.com>
2024-05-18 16:53:18 +0300

    initial commit and creation of models

---
+++ api/Models/Comment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        public Guid? StockId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        //Navigation property to allow us to navigate within the model relationships
+        public Stock? Stock { get; set; }
+
+    }
+}
