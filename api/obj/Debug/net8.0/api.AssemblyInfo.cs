0ccc83cfe7740bef41faa4434ce33340a915b26e
kariru-k <karirukeith@gmail.com>
2024-05-19 21:16:49 +0300

    Added mapping from stock to stock dto in get methods

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06dcfeba56f12e7e054e1e0e9d9c46e7db4fb79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d2903c7f0beb81e142e44ee2a3fc2728fe145aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
