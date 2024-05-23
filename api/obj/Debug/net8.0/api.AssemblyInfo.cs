4a81c71be064e0bffd5d3accc4880557acd7980f
kariru-k <karirukeith@gmail.com>
2024-05-23 12:53:58 +0300

    Added redis caching package

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c07f68dea5b76bac7ebc1b9ee6c1c86614c4080")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8eb77b6f4ee6878630c9633e12a35e5db96fc1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
